5909d86b0f29b1047f60f5a6ad599849d0502d6b
Mishal Turkane <95625543+MishalTurkane2004@users.noreply.github.com>
2023-03-24 16:09:02 +0530

    Add files via upload

---
+++ StackSIze.java
@@ -0,0 +1,10 @@
+package dsa.mishaldoughts;
+
+import java.util.Stack;
+public class StackSIze {
+    public static void main(String[] args) {
+        Stack <Integer> st=new Stack<>();
+        st.add(10);
+        System.out.println(st.size());
+    }
+}
